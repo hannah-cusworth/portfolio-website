1f86e7126d0345260753f61fa09875b6edf84087
Bewick <hanncus430@gmail.com>
2020-04-13 23:05:53 +0100

    added carousel

---
+++ assets/js/carousel.js
@@ -0,0 +1,33 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  var text = document.getElementsByClassName("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  for (i = 0; i < dots.length; i++) {
+    text[i].style.display="none";
+}
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  text[slideIndex-1].style.display = "block";
+}
